9bdc81d436aba02f987c25bb37e13c30d33a1eec
DisturbedCherry <jakub.t.wislicki@gmail.com>
2024-05-19 22:02:26 +0200

    Created resourceNotFoundException file

---
+++ src/main/java/com/example/timeder/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.timeder.exception;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
