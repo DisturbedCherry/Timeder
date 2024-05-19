c2cc916bf4e3e6a2e09d109d6c7e2a722e2b5b43
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-05-19 23:05:10 +0200

    Add data annotation

--- src/main/java/com/example/timeder/notification/Notification.java
+++ src/main/java/com/example/timeder/notification/Notification.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
+
 import java.time.LocalDate;
 
 @Entity
 @Table(name="notifications")
+@Data
 public class Notification {
     @Id
     @GeneratedValue
