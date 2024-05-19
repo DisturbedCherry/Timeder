3b326621e70a23463428c1c0a8a6b46ebefc7933
DisturbedCherry <jakub.t.wislicki@gmail.com>
2024-05-19 22:04:27 +0200

    Added misc project files

---
+++ src/test/java/com/example/timeder/TimederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
