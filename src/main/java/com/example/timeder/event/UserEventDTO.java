68f001ea60946934613e6d59fa0a708a81632f40
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-05-20 12:11:24 +0200

    Create UserEventDTO

---
+++ src/main/java/com/example/timeder/event/UserEventDTO.java
@@ -0,0 +1,10 @@
+package com.example.timeder.event;
+
+import lombok.Data;
+
+@Data
+public class UserEventDTO {
+    private Integer index;
+    private String firstName;
+    private String lastName;
+}
