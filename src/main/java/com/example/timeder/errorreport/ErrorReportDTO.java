fc37b653d96933a7a9c69a97df81452ed231b948
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-05-25 22:36:41 +0200

    Add CreateErrorReportDTO, refactor ErrorReportDTO

--- src/main/java/com/example/timeder/errorreport/ErrorReportDTO.java
+++ src/main/java/com/example/timeder/errorreport/ErrorReportDTO.java
@@ -1,18 +1,19 @@
 package com.example.timeder.errorreport;
 
-import com.example.timeder.user.User;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Data
+
 @AllArgsConstructor
+@Getter
+@Setter
 public class ErrorReportDTO {
-
-    private User sender;
-    ErrorReportStatus status;
-    private LocalDate dateTime;
+    Integer id;
+    Integer userId;
     String content;
-
+    String status;
+    LocalDate dateTime;
 }
