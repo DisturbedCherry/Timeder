3c224b4467b5c7d5f37d33d7a4b75c14a8ddd4dd
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-06-01 11:01:09 +0200

    Add token signature

--- src/main/java/com/example/timeder/config/JwtService.java
+++ src/main/java/com/example/timeder/config/JwtService.java
@@ -42,7 +42,8 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1_000 * 60 * 60)) // 1 hour                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .setExpiration(new Date(System.currentTimeMillis() + 1_000 * 60 * 60)) // 1 hour
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
