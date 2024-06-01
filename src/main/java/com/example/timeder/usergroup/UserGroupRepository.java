1067959c0c27049ba79cf8b5ec9df491b7ab22ab
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-06-02 00:33:40 +0200

    Add user group repository

--- src/main/java/com/example/timeder/usergroup/UserGroupRepository.java
+++ src/main/java/com/example/timeder/usergroup/UserGroupRepository.java
@@ -1,6 +1,11 @@
 package com.example.timeder.usergroup;
 
+import com.example.timeder.group.Group;
+import com.example.timeder.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 public interface UserGroupRepository extends JpaRepository<UserGroup, UserGroupId> {
+    Optional<UserGroup> findByUserAndGroup(User user, Group group);
 }
+
