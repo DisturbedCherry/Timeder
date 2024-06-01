4b5de971285b3d5fd4c2f666da187302516a7472
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-06-02 00:33:28 +0200

    Add group dto

---
+++ src/main/java/com/example/timeder/group/DeleteUserGroupDTO.java
@@ -0,0 +1,11 @@
+package com.example.timeder.group;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeleteUserGroupDTO {
+    private Integer groupId;
+    private Long userIndex;
+}
