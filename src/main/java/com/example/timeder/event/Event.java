bd1477e3e0a18fdbafbff04cf41755e15860fa03
JagodaJakub <kubajagjag@gmail.com>
2024-06-03 03:32:06 +0200

    Changed members to userEvents and added groupEvents, corrected table joins

--- src/main/java/com/example/timeder/event/Event.java
+++ src/main/java/com/example/timeder/event/Event.java
@@ -1,19 +1,24 @@
 package com.example.timeder.event;
 
+import com.example.timeder.groupevent.GroupEvent;
 import com.example.timeder.user.User;
+import com.example.timeder.userevent.UserEvent;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "events")
 @Data
+@AllArgsConstructor
 public class Event {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Integer id;
 
     private String name;
@@ -27,21 +32,26 @@ public class Event {
     @JoinColumn(name = "user_id")
     private User owner;
 
-    @ManyToMany
-    @JoinTable(
-            name = "event_members",
-            joinColumns = @JoinColumn(name = "event_id"),
-            inverseJoinColumns = @JoinColumn(name = "user_id")
-    )
-    private List<User> members;
+    // One-to-many relationship from Event to UserEvent
+    // cascade -> will delete all associated UserEvent rows when Event is deleted
+    @OneToMany(mappedBy = "event", fetch = FetchType.EAGER)
+    private List<UserEvent> userEvents;
 
-    public Event(String name, LocalDate startDateTime, Boolean isPrivate, String description, String localization, String photoFilePath) {
+    // One-to-many relationship from Event to GroupEvent
+    // cascade -> will delete all associated GroupEvent rows when Event is deleted
+    @OneToMany(mappedBy = "event", fetch = FetchType.EAGER)
+    private List<GroupEvent> groupEvents;
+
+    public Event(String name, LocalDate startDateTime, Boolean isPrivate, String description, String localization, String photoFilePath, User owner) {
         this.name = name;
         this.startDateTime = startDateTime;
         this.isPrivate = isPrivate;
         this.description = description;
         this.localization = localization;
         this.photoFilePath = photoFilePath;
+        this.owner = owner;
+        this.userEvents = new ArrayList<>();
+        this.groupEvents = new ArrayList<>();
     }
 
     public Event() {
