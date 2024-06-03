1ea62846470328ae78c1383ca0f33de01147fa81
JagodaJakub <kubajagjag@gmail.com>
2024-06-03 03:33:42 +0200

    corrected eventDTO

--- src/main/java/com/example/timeder/event/EventDTO.java
+++ src/main/java/com/example/timeder/event/EventDTO.java
@@ -2,18 +2,22 @@ package com.example.timeder.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EventDTO {
 
+    private Integer id;
     private String name;
     private LocalDate startDateTime;
     private Boolean isPrivate;
     private String description;
     private String localization;
     private String photoFilePath;
+    private Long ownerId;
 
 }
