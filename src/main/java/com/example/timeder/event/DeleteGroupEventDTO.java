55b9a15cd34075daa4510cf4bc8d6d9b4f696ccc
JagodaJakub <kubajagjag@gmail.com>
2024-06-03 03:30:45 +0200

    Added DeleteGroupEventDTO

---
+++ src/main/java/com/example/timeder/event/DeleteGroupEventDTO.java
@@ -0,0 +1,13 @@
+package com.example.timeder.event;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeleteGroupEventDTO {
+
+    private Integer eventId;
+    private Integer groupId;
+
+}
