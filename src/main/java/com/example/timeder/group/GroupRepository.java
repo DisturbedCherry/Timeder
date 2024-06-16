09072f769b81ca33de87b664e31d42260bae92f5
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-06-16 17:32:30 +0200

    Add check for unique join code and check if group is full

--- src/main/java/com/example/timeder/group/GroupRepository.java
+++ src/main/java/com/example/timeder/group/GroupRepository.java
@@ -4,4 +4,5 @@ package com.example.timeder.group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupRepository extends JpaRepository<Group, Integer> {
+    boolean existsByJoinCode(String joinCode);
 }
