c7896956dbae81277264d21ba29c691858011891
JagodaJakub <kubajagjag@gmail.com>
2024-06-16 19:27:10 +0200

    added findByName to GroupRepository

--- src/main/java/com/example/timeder/group/GroupRepository.java
+++ src/main/java/com/example/timeder/group/GroupRepository.java
@@ -2,5 +2,8 @@ package com.example.timeder.group;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GroupRepository extends JpaRepository<Group, Integer> {
+    Optional<Group> findByName(String name);
 }
